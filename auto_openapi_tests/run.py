e2e3ac5ff52d8a1974ff8bc7204a0c3b6f2f748e
joopitz <jonas.opitz@live.de>
2023-08-28 15:03:39 +0200

    feat: add option to specify cache folder

--- auto_openapi_tests/run.py
+++ auto_openapi_tests/run.py
@@ -1,12 +1,29 @@
 import argparse
+import os
 import sys
+from typing import Literal, Optional
 
 import pytest
 
 from auto_openapi_tests._version import __version__
 
 
-def run(api: str, openapi_loc: str, skip_endpoints: list[str]) -> int | pytest.ExitCode:
+def run(
+    api: str,
+    openapi_loc: str,
+    skip_endpoints: list[str],
+    cache_dir: Optional[str] | Literal["disabled"] = None,
+) -> int | pytest.ExitCode:
+    cache_dir = (
+        os.environ.get("PYTEST_CACHE_DIR", ".pytest_cache")
+        if not cache_dir
+        else cache_dir
+    )
+
+    if cache_dir == "disabled":
+        cache_args = ["-p", "no:cacheprovider"]
+    else:
+        cache_args = ["-o", f"cache_dir={cache_dir}"]
     return pytest.main(
         [
             "-s",
@@ -18,6 +35,7 @@ def run(api: str, openapi_loc: str, skip_endpoints: list[str]) -> int | pytest.E
             "--skip-endpoints",
             str(skip_endpoints),
         ]
+        + cache_args
     )
 
 
@@ -46,6 +64,12 @@ def main():
         type=str,
     )
     parser.add_argument(
+        "--cache-dir",
+        action="store",
+        default=".cache",
+        help="The cache directory to be used. Set to 'disabled' to disable caching.",
+    )
+    parser.add_argument(
         "--version",
         action="version",
         version="%(prog)s {version}".format(version=__version__),
@@ -54,7 +78,14 @@ def main():
     # read passed CLI arguments
     args = parser.parse_args()
 
-    sys.exit(run(args.api, args.spec_loc, args.skip_endpoints))
+    sys.exit(
+        run(
+            api=args.api,
+            openapi_loc=args.spec_loc,
+            skip_endpoints=args.skip_endpoints,
+            cache_dir=args.cache_dir,
+        )
+    )
 
 
 if __name__ == "__main__":
